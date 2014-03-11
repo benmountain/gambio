107dd6f1012471e4920fab63067dd6b6deeaf7c4
Robert Gold <rgd@easymarketing.de>
2014-03-11 17:11:03 +0100

    fix conversion tracker

--- api/easymarketing/conversion_tracker.php
+++ api/easymarketing/conversion_tracker.php
@@ -10,21 +10,28 @@
    Released under the GNU General Public License
    ---------------------------------------------------------------------------------------*/
 
-require_once (DIR_FS_INC.'get_external_content.inc.php');
+if (basename($PHP_SELF) == FILENAME_CHECKOUT_SUCCESS && defined('MODULE_EASYMARKETING_STATUS') && MODULE_EASYMARKETING_STATUS == 'True') 
+{
+    // include easymarketing configuration
+    require_once(DIR_FS_CATALOG.'api/easymarketing/includes/config.php');
 
-if (basename($PHP_SELF) == FILENAME_CHECKOUT_SUCCESS && defined('MODULE_EASYMARKETING_STATUS') && MODULE_EASYMARKETING_STATUS == 'True') {
+  $ch = curl_init();
 
-  // include easymarketing configuration
-  require_once(DIR_FS_CATALOG.'api/easymarketing/includes/config.php');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_URL, 'https://'.EASYMARKETING_API_URL.'/conversion_tracker?website_url='.str_replace(array('http://', 'https://'), '', HTTP_SERVER).'&access_token='.MODULE_EASYMARKETING_ACCESS_TOKEN);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json', 'Accept: application/json'));
+  
+    $response = curl_exec($ch);
 
-  // get conversion tracker
-  $conversion_tracker = get_external_content(EASYMARKETING_API_URL.'/conversion_tracker/'.str_replace(array('http://', 'https://'), '', HTTP_SERVER).'?access_token='.MODULE_EASYMARKETING_ACCESS_TOKEN, 3, false);  
-  $response = json_decode($conversion_tracker);
+    curl_close($ch);
 
-  // print out code
-  if (is_object($response)) {
-    echo $response->code;
-    echo $response->img;
-  }
+  $conversion_tracker_data = json_decode($response, true);
 
+    // print out conversion tracker code
+    if (is_array($conversion_tracker_data)) 
+    {
+      echo $conversion_tracker_data['code'];
+      echo $conversion_tracker_data['img'];
+    }
 }
+
