3ccaa1b5ec184dd844c97dc99ce6797d70454d51
Florian Ressel <info@k-30.de>
2016-11-21 01:09:45 +0100

    fix typos

--- lang/german/user_sections/admin/easymarketing.lang.inc.php
+++ lang/german/user_sections/admin/easymarketing.lang.inc.php
@@ -11,8 +11,8 @@
    @author		Florian Ressel <florian.ressel@easymarketing.de>
 
    @file       lang/german/user_sections/admin/easymarketing.lang.inc.php
-   @version    v3.1.0
-   @updated    20.11.2016 - 20:38
+   @version    v3.0.0
+   @updated    20.11.2016 - 19:31
    ---------------------------------------------------------------------------------------*/
 
 $t_language_text_section_content_array = array
